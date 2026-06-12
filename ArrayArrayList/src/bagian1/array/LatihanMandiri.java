5313b884bf2973a3c816662a193484232983e388
Chintya Mahfuzah <chhintyaya@gmail.com>
2026-06-12 12:22:01 +0800

    Update LatihanMandiri.java

--- ArrayArrayList/src/bagian1/array/LatihanMandiri.java
+++ ArrayArrayList/src/bagian1/array/LatihanMandiri.java
@@ -5,16 +5,11 @@ public class LatihanMandiri {
     public static void main(String [] args){
         double[] suhu = {27.0, 28.0, 29.0, 31.0, 32.0, 33.00,};
         
-        double max = suhu [0];
-        double min = suhu [0];
+        double max = suhu [0],min = suhu [0];
         
         for (double s : suhu){
-            if (s > max){
-                max = s;
-            }
-            if (s < min){
-                min = s;
-            }
+            if (s > max) max = s;
+            if (s < min) min = s;
         }
         System.out.println("=== HASIL SUHU HARIAN ===");
         System.out.println("Tertinggi : " + max);
